0ec8c188482d50c46b7b8cb453e0d1082c709e60
Arnold Kibira <arnold.ril3y@gmail.com>
2022-02-04 15:28:23 +0300

    Set up navigation
    Set up pages and configure sidebar links to direct to pages

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
 import React from 'react';
-import { useAppSelector } from '../app/hooks';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { toggleSidebar } from '../features/navigation/sideBarSlice';
 
 
 const SideNav: React.FC = () => {
   const isOpen = useAppSelector(state => state.sidebar.isOpen)
+  const dispatch = useAppDispatch()
+
+  const toggleSidebarHandler = () => {
+    dispatch(toggleSidebar())
+  }
 
   return (
     <div className={`z-10 min-h-screen max-h-screen w-3/4 top-0 left-0 fixed bg-gray-50 shadow-xl ${isOpen ? "translate-x-0 " : "-translate-x-full"} ease-in-out duration-300 p-4 flex flex-col justify-items-center space-y-40 items-center`
@@ -12,9 +19,15 @@ const SideNav: React.FC = () => {
         <p className="text-3xl ">LOGO HERE</p>
       </div>
       <div className="w-full flex-col space-y-20 p-4 ">
-        <p className="text-center text-3xl tracking-wider ">Home</p>
-        <p className="text-center text-3xl tracking-wider ">About</p>
-        <p className="text-center text-3xl tracking-wider ">Contact</p>
+        <Link href="/" >
+          <p className="text-center text-3xl tracking-wider" onClick={toggleSidebarHandler}>Home</p>
+        </Link>
+        <Link href="/about" >
+          <p className="text-center text-3xl tracking-wider" onClick={toggleSidebarHandler}>About</p>
+        </Link>
+        <Link href="/contact" >
+          <p className="text-center text-3xl tracking-wider" onClick={toggleSidebarHandler}>Contact Us</p>
+        </Link>
       </div>
     </div >
   )
