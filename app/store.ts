b0bae4ef244b2acecc4475ef00ae56a1568b66b5
Arnold Kibira <arnold.ril3y@gmail.com>
2022-02-04 15:05:26 +0300

    Configure redux store
    Configure redux store with sidebar reducer

--- app/store.ts
+++ app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import sideBarReducer from "../features/navigation/sideBarSlice"
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    sidebar: sideBarReducer
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
